abf9d7c2549455e488a948c6d8e95e56430a93b4
jaesonwatts <jaeson.watts@gmail.com>
2020-08-10 13:53:24 -0600

    index/create built, routes working

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -1,10 +1,27 @@
 class Api::V1::MembersController < ApplicationController
   
   def index
-    @members = Member.all
-    render json: @members
+    members = Member.all
+    render json: members
   end
 
-  
+  def create
+    member = Member.new(:name, :email, :admin_level, :password, :uid)
+    if member.save
+      render json: member, status: :accepted
+    else
+      render json: {errors: member.errors.full_messages}, status: :unprocessible_entity
+    end
+
+  end
+
+
+
+  private
+
+  def member_params(*args)
+    params.require(:member).permit(*args)
+  end
+
 
 end
