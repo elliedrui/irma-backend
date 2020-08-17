35ea8ec388d53e1b3d8539ae066d3886597bdd4a
jaesonwatts <jaeson.watts@gmail.com>
2020-08-16 23:20:18 -0600

    cors config alteration to allow localhost vs ip address

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Irma
 
   Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://127.0.0.1:5500'
+      origins 'http://127.0.0.1:5500', 'http://localhost:5500', 'localhost:5500'
 
       resource '*',
         headers: :any,
